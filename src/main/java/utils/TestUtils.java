bb12dd06ae90b9ed57ec89137e8f1573ac8739c5
Nischal <nischal.pradhan20@gmail.com>
2024-05-09 23:11:18 +0100

    [SLAT-007] test utils class updated, fix done to run on jenkins

--- src/main/java/utils/TestUtils.java
+++ src/main/java/utils/TestUtils.java
@@ -11,7 +11,7 @@ public class TestUtils {
 
         Properties config = new Properties();
         String user_dir=(System.getProperty("user.dir"));
-        try (FileInputStream fileInputStream = new FileInputStream(user_dir+"\\src\\test\\resources\\SwagsTestData.properties")) {
+        try (FileInputStream fileInputStream = new FileInputStream(user_dir+"\\src\\test\\resources\\SwagLabsTestData.properties")) {
             config.load(fileInputStream);
         } catch (IOException e) {
             e.printStackTrace();
